3ff17a9ae239a0f48e7165ddf582ca889366278a
Muhammad Faisal Peerzada <mohammadfaisalpirzada@gmail.com>
2024-12-08 23:32:00 +0500

    in progress

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -4,32 +4,34 @@ import styles from './SearchBar.module.css'; // Import CSS module
 
 export default function SearchBar() {
     return (
-        <div className={styles.inputContainer}>
-            {/* Search Icon */}
-            <div className={styles.icon}>
-                <Image
-                    src="/images/icons/search-normal.png" // Use Next.js Image component for optimization
-                    alt="Search Icon"
-                    width={16} // Adjust icon size
-                    height={16}
-                />
-            </div>
+        <div className={styles.wrapper}>
+            <div className={styles.inputContainer}>
+                {/* Search Icon */}
+                <div className={styles.icon}>
+                    <Image
+                        src="/images/icons/search-normal.png" // Path to search icon
+                        alt="Search Icon"
+                        width={16} // Adjust icon size
+                        height={16}
+                    />
+                </div>
 
-            {/* Input Field */}
-            <input
-                type="text"
-                className={styles.input}
-                placeholder="Search something here"
-            />
-
-            {/* Settings Icon */}
-            <div className={styles.settingsIcon}>
-                <Image
-                    src="/images/icons/filter.png" // Use Next.js Image component for optimization
-                    alt="Filter Icon"
-                    width={16} // Adjust icon size
-                    height={16}
+                {/* Input Field */}
+                <input
+                    type="text"
+                    className={styles.input}
+                    placeholder="Search something here"
                 />
+
+                {/* Settings Icon */}
+                <div className={styles.settingsIcon}>
+                    <Image
+                        src="/images/icons/filter.png" // Path to filter icon
+                        alt="Filter Icon"
+                        width={16} // Adjust icon size
+                        height={16}
+                    />
+                </div>
             </div>
         </div>
     );
